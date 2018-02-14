f634ab0c712c5ee4e7ba0f8f43123405b4345176
victordelval <victor.val.andres@gmail.com>
2018-02-14 15:54:22 +0100

    Fixes object property name

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
 class LabelButton extends React.Component {
     render() {
-        const name = this.props.item.content;
+        const name = this.props.item.name;
         const code = this.props.item.code;
         return (
             <label className="button"
